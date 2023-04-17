6e03b7d0bc63ae37570e00babc0b814ce3a547c7
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-17 19:17:26 +0530

    Update datastore.php

--- src/datastore.php
+++ src/datastore.php
@@ -1,39 +1,29 @@
 <?php
-$method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-$datastore_scope = array_shift($request);
-$datastore_name = array_shift($request);
-
-if ($method == 'POST') {
-    $data = json_decode(file_get_contents('php://input'), true);
-    if (!is_array($data)) {
-        http_response_code(400);
-        echo json_encode(array('error' => 'Invalid JSON data'));
-        exit;
-    }
-    $filename = $datastore_scope . '_' . $datastore_name . '_' . $request[0] . '.json';
-    if (!file_exists($filename)) {
-        file_put_contents($filename, '{}');
-    }
-    $json = json_decode(file_get_contents($filename), true);
-    $json[$request[0]] = $data;
-    file_put_contents($filename, json_encode($json));
-    echo json_encode(array('success' => true));
-} elseif ($method == 'GET') {
-    $filename = $datastore_scope . '_' . $datastore_name . '_' . $request[0] . '.json';
-    if (!file_exists($filename)) {
-        echo json_encode(array('error' => 'DataStore not found'));
-        exit;
-    }
-    $json = json_decode(file_get_contents($filename), true);
-    if (isset($request[1])) {
-        echo json_encode($json[$request[1]]);
+#Signed By ARD at 19:17 IST.
+if ($_GET["scope"] && $_GET["name"]) {
+    $scope = $_GET["scope"];
+    $name = $_GET["name"];
+    $filename = "datastore/" . $scope . "/" . $name . ".json";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $data = json_decode(file_get_contents('php://input'), true);
+        if ($data) {
+            if (!file_exists(dirname($filename))) {
+                mkdir(dirname($filename), 0777, true);
+            }
+            file_put_contents($filename, json_encode($data));
+            http_response_code(204);
+        } else {
+            http_response_code(400);
+        }
     } else {
-        echo json_encode($json);
+        if (file_exists($filename)) {
+            $data = json_decode(file_get_contents($filename), true);
+            echo json_encode($data);
+        } else {
+            http_response_code(404);
+        }
     }
 } else {
-    http_response_code(405);
-    header('Allow: GET, POST');
-    echo json_encode(array('error' => 'Method not allowed'));
+    http_response_code(400);
 }
 ?>
