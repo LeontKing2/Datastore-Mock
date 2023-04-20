3e55156ebf80a79ca9dc9181ef348c7973e4092d
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-20 20:36:38 +0530

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,10 +1,17 @@
 import json
 from flask import Flask, request
-## Signed By ARD on 20/04/2023 just 10 days before his birthday on 19:47 IST
+import os
+
 app = Flask(__name__)
 
 # JSON file to store data
 DATA_FILE = 'data.json'
+
+# Function to check if JSON file exists, and create it if not
+def check_json_file():
+    if not os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'w') as f:
+            json.dump({}, f)
 
 @app.route('/save_data', methods=['POST'])
 def save_data():
@@ -13,21 +20,23 @@ def save_data():
     scope = payload['scope']
     key = payload['key']
     value = payload['value']
-    
+
     # Load existing data from the JSON file
     with open(DATA_FILE, 'r') as f:
         data = json.load(f)
-    
-    # Save data to the JSON file
+
+    # Create scope and name if not exist
     if scope not in data:
         data[scope] = {}
     if name not in data[scope]:
         data[scope][name] = {}
+
+    # Save data to the JSON file
     data[scope][name][key] = value
-    
+
     with open(DATA_FILE, 'w') as f:
         json.dump(data, f, indent=4)
-    
+
     print(f'Saving data: name={name}, scope={scope}, key={key}, value={value}')
     return json.dumps({'success': True})
 
@@ -37,21 +46,22 @@ def get_data():
     name = payload['name']
     scope = payload['scope']
     key = payload['key']
-    
+
     # Load data from the JSON file
     with open(DATA_FILE, 'r') as f:
         data = json.load(f)
-    
+
     # Retrieve data from the JSON file
     if scope in data and name in data[scope] and key in data[scope][name]:
         value = data[scope][name][key]
     else:
         value = None
-    
+
     print(f'Getting data: name={name}, scope={scope}, key={key}, value={value}')
-    
+
     # Return the retrieved value or None
     return json.dumps({'value': value})
 
 if __name__ == '__main__':
+    check_json_file() # Check if JSON file exists and create if not
     app.run(port=8001, debug=True)
