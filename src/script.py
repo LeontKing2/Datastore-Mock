10a725bfbf919df4d4ee6b7c6a104bcb36b39113
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-21 21:53:55 +0530

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,55 +1,56 @@
-from flask import Flask, request, jsonify
 import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-DATA_FILE = "datastore.json"  # File to store data
+# File to store the data
+DATASTORE_FILE = 'datastore.json'
 
-def load_data():
+# Load initial data from the file
+try:
+    with open(DATASTORE_FILE, 'r') as f:
+        datastore = json.load(f)
+except FileNotFoundError:
+    datastore = {}
+
+@app.route('/datastore', methods=['POST'])
+def datastore_endpoint():
     try:
-        with open(DATA_FILE, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return {}
-
-def save_data(data):
-    with open(DATA_FILE, 'w') as f:
-        json.dump(data, f, indent=4)
-
-@app.route('/save_data', methods=['POST'])
-def save_data_route():
-    payload = request.get_json()
-    name = payload['name']
-    scope = payload['scope']
-    key = payload['key']
-    value = payload['value']
-
-    data = load_data()
-
-    if scope not in data:
-        data[scope] = {}
-    if name not in data[scope]:
-        data[scope][name] = {}
-
-    # Update value instead of duplicating
-    data[scope][name][key] = value
-
-    save_data(data)
-
-    return jsonify({'success': True})
-
-@app.route('/get_data', methods=['POST'])
-def get_data_route():
-    payload = request.get_json()
-    name = payload['name']
-    scope = payload['scope']
-    key = payload['key']
-
-    data = load_data()
-
-    value = data.get(scope, {}).get(name, {}).get(key)
-
-    return jsonify({'value': value})
+        data = request.json
+        method = data['method']
+        key = data['key']
+        if method == 'SetAsync':
+            value = data['value']
+            datastore[key] = value
+            with open(DATASTORE_FILE, 'w') as f:
+                json.dump(datastore, f)
+            return jsonify({'success': True})
+        elif method == 'GetAsync':
+            value = datastore.get(key)
+            return jsonify({'success': True, 'value': value})
+        elif method == 'UpdateAsync':
+            transform_func = data['transform_func']
+            if key in datastore:
+                value = datastore[key]
+                new_value = transform_func(value)
+                datastore[key] = new_value
+                with open(DATASTORE_FILE, 'w') as f:
+                    json.dump(datastore, f)
+                return jsonify({'success': True, 'value': new_value})
+            else:
+                return jsonify({'success': False, 'error': 'Key not found'})
+        elif method == 'RemoveAsync':
+            if key in datastore:
+                del datastore[key]
+                with open(DATASTORE_FILE, 'w') as f:
+                    json.dump(datastore, f)
+                return jsonify({'success': True})
+            else:
+                return jsonify({'success': False, 'error': 'Key not found'})
+        else:
+            return jsonify({'success': False, 'error': 'Invalid method'})
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    app.run(port=8001, debug=True)
