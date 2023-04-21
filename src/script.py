5a668143c1d6b19e4dfb59b6bd878b5620134648
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-21 22:00:48 +0530

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,56 +1,48 @@
-import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# File to store the data
-DATASTORE_FILE = 'datastore.json'
+# Datastore to store key-value pairs
+datastore = {}
 
-# Load initial data from the file
-try:
-    with open(DATASTORE_FILE, 'r') as f:
-        datastore = json.load(f)
-except FileNotFoundError:
-    datastore = {}
+@app.route('/datastore/set', methods=['POST'])
+def set_data():
+    key = request.form.get('key')
+    value = request.form.get('value')
+    if key is not None and value is not None:
+        datastore[key] = value
+        return 'OK', 200
+    else:
+        return 'Bad Request', 400
 
-@app.route('/datastore', methods=['POST'])
-def datastore_endpoint():
-    try:
-        data = request.json
-        method = data['method']
-        key = data['key']
-        if method == 'SetAsync':
-            value = data['value']
-            datastore[key] = value
-            with open(DATASTORE_FILE, 'w') as f:
-                json.dump(datastore, f)
-            return jsonify({'success': True})
-        elif method == 'GetAsync':
-            value = datastore.get(key)
-            return jsonify({'success': True, 'value': value})
-        elif method == 'UpdateAsync':
-            transform_func = data['transform_func']
-            if key in datastore:
-                value = datastore[key]
-                new_value = transform_func(value)
-                datastore[key] = new_value
-                with open(DATASTORE_FILE, 'w') as f:
-                    json.dump(datastore, f)
-                return jsonify({'success': True, 'value': new_value})
-            else:
-                return jsonify({'success': False, 'error': 'Key not found'})
-        elif method == 'RemoveAsync':
-            if key in datastore:
-                del datastore[key]
-                with open(DATASTORE_FILE, 'w') as f:
-                    json.dump(datastore, f)
-                return jsonify({'success': True})
-            else:
-                return jsonify({'success': False, 'error': 'Key not found'})
-        else:
-            return jsonify({'success': False, 'error': 'Invalid method'})
-    except Exception as e:
-        return jsonify({'success': False, 'error': str(e)})
+@app.route('/datastore/get', methods=['GET'])
+def get_data():
+    key = request.args.get('key')
+    if key is not None and key in datastore:
+        return jsonify({'value': datastore[key]}), 200
+    else:
+        return 'Not Found', 404
+
+@app.route('/datastore/update', methods=['POST'])
+def update_data():
+    key = request.form.get('key')
+    value = request.form.get('value')
+    if key is not None and key in datastore and value is not None:
+        current_value = datastore[key]
+        current_value.update(value)
+        datastore[key] = current_value
+        return 'OK', 200
+    else:
+        return 'Bad Request', 400
+
+@app.route('/datastore/remove', methods=['POST'])
+def remove_data():
+    key = request.form.get('key')
+    if key is not None and key in datastore:
+        del datastore[key]
+        return 'OK', 200
+    else:
+        return 'Not Found', 404
 
 if __name__ == '__main__':
-    app.run(port=8001, debug=True)
+    app.run(host='localhost', port=8001)
