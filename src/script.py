207d2efe1f84a7defc1b1503d15c738ae44d601d
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-22 15:58:52 +0530

    Rewritten the entire thing? idk

--- src/script.py
+++ src/script.py
@@ -1,63 +1,87 @@
-from flask import Flask, request, jsonify
 import json
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# File path to store data in JSON format
-datastore_file = 'datastore.json'
+# Route for saving data to the server
+@app.route('/save', methods=['POST'])
+def save():
+    try:
+        data = request.get_json()
+        store_name = data.get('storeName')
+        store_scope = data.get('storeScope')
+        key = data.get('key')
+        value = data.get('value')
 
-# Load initial data from JSON file
-try:
-    with open(datastore_file, 'r') as file:
-        datastore = json.load(file)
-except FileNotFoundError:
-    datastore = {}
+        # Create a dictionary to store the data
+        store_data = {}
+        store_data['storeName'] = store_name
+        store_data['storeScope'] = store_scope
+        store_data['data'] = {}
 
-@app.route('/datastore/set', methods=['POST'])
-def set_data():
-    key = request.form.get('key')
-    value = request.form.get('value')
-    if key is not None and value is not None:
-        datastore[key] = value
-        save_datastore_to_file()
-        return 'OK', 200
-    else:
-        return 'Bad Request', 400
+        # Check if the JSON file exists, if not, create it
+        try:
+            with open('data.json', 'r') as f:
+                store_data = json.load(f)
+        except FileNotFoundError:
+            with open('data.json', 'w') as f:
+                json.dump(store_data, f)
 
-@app.route('/datastore/get', methods=['GET'])
-def get_data():
-    key = request.args.get('key')
-    if key is not None and key in datastore:
-        return jsonify({'value': datastore[key]}), 200
-    else:
-        return 'Not Found', 404
+        # Save the data to the JSON file
+        with open('data.json', 'r+') as f:
+            store_data = json.load(f)
+            if store_name not in store_data['data']:
+                store_data['data'][store_name] = {}
+            if store_scope not in store_data['data'][store_name]:
+                store_data['data'][store_name][store_scope] = {}
+            store_data['data'][store_name][store_scope][key] = value
+            f.seek(0)
+            json.dump(store_data, f)
+            f.truncate()
 
-@app.route('/datastore/update', methods=['POST'])
-def update_data():
-    key = request.form.get('key')
-    value = request.form.get('value')
-    if key is not None and key in datastore and value is not None:
-        current_value = datastore[key]
-        current_value.update(value)
-        datastore[key] = current_value
-        save_datastore_to_file()
-        return 'OK', 200
-    else:
-        return 'Bad Request', 400
+        # Return a JSON response indicating success
+        response = {
+            'success': True
+        }
+        return jsonify(response)
 
-@app.route('/datastore/remove', methods=['POST'])
-def remove_data():
-    key = request.form.get('key')
-    if key is not None and key in datastore:
-        del datastore[key]
-        save_datastore_to_file()
-        return 'OK', 200
-    else:
-        return 'Not Found', 404
+    except Exception as e:
+        # Return a JSON response indicating failure with an error message
+        response = {
+            'success': False,
+            'error': str(e)
+        }
+        return jsonify(response), 500
 
-def save_datastore_to_file():
-    with open(datastore_file, 'w') as file:
-        json.dump(datastore, file)
+# Route for loading data from the server
+@app.route('/load', methods=['POST'])
+def load():
+    try:
+        data = request.get_json()
+        store_name = data.get('storeName')
+        store_scope = data.get('storeScope')
+        key = data.get('key')
+
+        # Load the data from the JSON file
+        with open('data.json', 'r') as f:
+            store_data = json.load(f)
+            value = store_data['data'].get(store_name, {}).get(store_scope, {}).get(key)
+
+        # Return a JSON response with the loaded data
+        response = {
+            'success': True,
+            'value': value
+        }
+        return jsonify(response)
+
+    except Exception as e:
+        # Return a JSON response indicating failure with an error message
+        response = {
+            'success': False,
+            'error': str(e)
+        }
+        return jsonify(response), 500
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8001)
+    app.run(debug=True, port=8001)  # Run the Flask app in debug mode on port 8001 for development
+## Rewritten THe Entire thing!
