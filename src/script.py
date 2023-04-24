7db75c007b19d9c2d978395437a96415ba0d3269
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-24 11:04:24 +0530

    yes

--- src/script.py
+++ src/script.py
@@ -8,19 +8,19 @@ app = Quart(__name__)
 @app.route('/load', methods=['POST'])
 async def load_data():
     data = await request.json
-    store_name = data['storeName']
-    store_scope = data['storeScope']
+    storeName = data['storeName']
+    storeScope = data['storeScope']
     key = data['key']
     
-    # Load data from JSON file or SQLite database based on store_scope
-    if store_scope == 'global':
+    # Load data from JSON file or SQLite database based on storeScope
+    if storeScope == 'global':
         with open('./global_data.json', 'r') as f:
             global_data = json.load(f)
         value = global_data.get(key)
-    elif store_scope == 'local':
+    elif storeScope == 'local':
         conn = sqlite3.connect('./local_data.db')
         cursor = conn.cursor()
-        cursor.execute("./SELECT value FROM local_data WHERE store_name=? AND key=?", (store_name, key))
+        cursor.execute("./SELECT value FROM local_data WHERE storeName=? AND key=?", (storeName, key))
         result = cursor.fetchone()
         value = result[0] if result else None
         conn.close()
@@ -33,23 +33,23 @@ async def load_data():
 @app.route('/save', methods=['POST'])
 async def save_data():
     data = await request.json
-    store_name = data['storeName']
-    store_scope = data['storeScope']
+    storeName = data['storeName']
+    storeScope = data['storeScope']
     key = data['key']
     value = data['value']
     
-    # Save data to JSON file or SQLite database based on store_scope
-    if store_scope == 'global':
+    # Save data to JSON file or SQLite database based on storeScope
+    if storeScope == 'global':
         with open('./global_data.json', 'r') as f:
             global_data = json.load(f)
         global_data[key] = value
         with open('./global_data.json', 'w') as f:
             json.dump(global_data, f)
-    elif store_scope == 'local':
+    elif storeScope == 'local':
         conn = sqlite3.connect('./local_data.db')
         cursor = conn.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS local_data (store_name TEXT, key TEXT, value TEXT)")
-        cursor.execute("INSERT OR REPLACE INTO local_data (store_name, key, value) VALUES (?, ?, ?)", (store_name, key, value))
+        cursor.execute("CREATE TABLE IF NOT EXISTS local_data (storeName TEXT, key TEXT, value TEXT)")
+        cursor.execute("INSERT OR REPLACE INTO local_data (storeName, key, value) VALUES (?, ?, ?)", (storeName, key, value))
         conn.commit()
         conn.close()
     else:
