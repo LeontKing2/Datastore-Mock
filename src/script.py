d36d9c6ec7010a9f18aac44ad1a010e69b54bcb4
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-24 10:59:10 +0530

    idk what i did but thank me.

--- src/script.py
+++ src/script.py
@@ -1,171 +1,61 @@
-import json
 from quart import Quart, request, jsonify
-import pyodbc
+import json
 import sqlite3
-
-# Storage Types = sqlite, sql_server, json
-storage_type = "json"
 
 app = Quart(__name__)
 
-def save_to_sql_server(store_name, store_scope, key, value):
-    # Establish a connection to SQL Server
-    conn = pyodbc.connect('DRIVER={SQL Server};'
-                          'SERVER=<your_server_name>;'
-                          'DATABASE=<your_database_name>;'
-                          'UID=<your_username>;'
-                          'PWD=<your_password>')
+# Endpoint for loading data
+@app.route('/load', methods=['POST'])
+async def load_data():
+    data = await request.json
+    store_name = data['storeName']
+    store_scope = data['storeScope']
+    key = data['key']
+    
+    # Load data from JSON file or SQLite database based on store_scope
+    if store_scope == 'global':
+        with open('./global_data.json', 'r') as f:
+            global_data = json.load(f)
+        value = global_data.get(key)
+    elif store_scope == 'local':
+        conn = sqlite3.connect('./local_data.db')
+        cursor = conn.cursor()
+        cursor.execute("./SELECT value FROM local_data WHERE store_name=? AND key=?", (store_name, key))
+        result = cursor.fetchone()
+        value = result[0] if result else None
+        conn.close()
+    else:
+        return jsonify({'success': False, 'error': 'Invalid storeScope'})
+    
+    return jsonify({'success': True, 'value': value})
 
-    # Create a cursor to interact with the database
-    cursor = conn.cursor()
-
-    # Construct the SQL query to insert data into the table
-    query = f"INSERT INTO {store_name} (store_scope, key, value) VALUES (?, ?, ?)"
-    cursor.execute(query, (store_scope, key, value))
-
-    # Commit the transaction and close the connection
-    conn.commit()
-    conn.close()
-
-# Function to load data from SQL Server
-def load_from_sql_server(store_name, store_scope, key):
-    # Establish a connection to SQL Server
-    conn = pyodbc.connect('DRIVER={SQL Server};'
-                          'SERVER=<your_server_name>;'
-                          'DATABASE=<your_database_name>;'
-                          'UID=<your_username>;'
-                          'PWD=<your_password>')
-
-    # Create a cursor to interact with the database
-    cursor = conn.cursor()
-
-    # Construct the SQL query to select data from the table
-    query = f"SELECT value FROM {store_name} WHERE store_scope=? AND key=?"
-    cursor.execute(query, (store_scope, key))
-
-    # Fetch the result and close the connection
-    result = cursor.fetchone()
-    conn.close()
-
-    # Return the value if found, otherwise return None
-    return result[0] if result else None
-
-# Function to save data to JSON file
-def save_to_json(store_name, store_scope, key, value):
-    try:
-        # Load the existing JSON file into memory, or create an empty dictionary if the file doesn't exist
-        try:
-            with open(f"{store_name}.json", 'r') as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            data = {}
-
-        # Update the data with the new key-value pair
-        if store_scope not in data:
-            data[store_scope] = {}
-        data[store_scope][key] = value
-
-        # Write the updated data back to the JSON file
-        with open(f"{store_name}.json", 'w') as file:
-            json.dump(data, file)
-    except Exception as e:
-        # Print an error message if an error occurs
-        print(f"Error: Failed to save to JSON file. {e}")
-
-# Function to load data from JSON file
-
-
-def load_from_json(store_name=None, store_scope=None, key=None):
-    if store_name is None:
-        store_name = "default_store_name"  # Set default store_name if not provided
-
-    if store_scope is None:
-        store_scope = "default_store_scope"  # Set default store_scope if not provided
-
-    try:
-        # Try to open the existing JSON file
-        with open(f"{store_name}.json", 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        # If the JSON file doesn't exist, create an empty dictionary and write it to a new file
-        data = {}
-        with open(f"{store_name}.json", 'w') as file:
-            json.dump(data, file)
-
-    # Return the value if found, otherwise return None
-    return data.get(store_scope, {}).get(key)
-
-
-# Function to save data to SQLite database
-def save_to_sqlite(store_name, store_scope, key, value):
-    # Establish a connection to SQLite database
-    conn = sqlite3.connect(f"{store_name}.db")
-    cursor = conn.cursor()
-
-    # Construct the SQL query to insert data into the table
-    query = f"INSERT INTO {store_name} (store_scope, key, value) VALUES (?, ?, ?)"
-    cursor.execute(query, (store_scope, key, value))
-
-    # Commit the transaction and close the connection
-    conn.commit()
-    conn.close()
-
-# Function to load data from SQLite database
-def load_from_sqlite(store_name, store_scope, key):
-    # Establish a connection to SQLite database
-    conn = sqlite3.connect(f"{store_name}.db")
-    cursor = conn.cursor()
-
-    # Construct the SQL query to select data from the table
-    query = f"SELECT value FROM {store_name} WHERE store_scope=? AND key=?"
-    cursor.execute(query, (store_scope, key))
-
-    # Fetch the result and close the connection
-    result = cursor.fetchone()
-    conn.close()
-
-    # Return the value if found, otherwise return None
-    return result[0] if result else None
-
-# Endpoint to save data
+# Endpoint for saving data
 @app.route('/save', methods=['POST'])
-async def save():
-    store_name = request.args.get('storeName')
-    store_scope = request.args.get('storeScope')
-    key = request.args.get('key')
-    value = request.args.get('value')
-
-    if storage_type == "json":
-        save_to_json(store_name, store_scope, key, value)
-    elif storage_type == "sql_server":
-        save_to_sql_server(store_name, store_scope, key, value)
-    elif storage_type == "sqlite":
-        save_to_sqlite(store_name, store_scope, key, value)
+async def save_data():
+    data = await request.json
+    store_name = data['storeName']
+    store_scope = data['storeScope']
+    key = data['key']
+    value = data['value']
+    
+    # Save data to JSON file or SQLite database based on store_scope
+    if store_scope == 'global':
+        with open('./global_data.json', 'r') as f:
+            global_data = json.load(f)
+        global_data[key] = value
+        with open('./global_data.json', 'w') as f:
+            json.dump(global_data, f)
+    elif store_scope == 'local':
+        conn = sqlite3.connect('./local_data.db')
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS local_data (store_name TEXT, key TEXT, value TEXT)")
+        cursor.execute("INSERT OR REPLACE INTO local_data (store_name, key, value) VALUES (?, ?, ?)", (store_name, key, value))
+        conn.commit()
+        conn.close()
     else:
-        return jsonify({'error': 'Invalid storage type'})
-
-    return jsonify({'result': 'Data saved successfully'})
-
-# Endpoint to load data
-@app.route('/load', methods=['GET'])
-async def load():
-    store_name = request.args.get('storeName')
-    store_scope = request.args.get('storeScope')
-    key = request.args.get('key')
-
-    if storage_type == "json":
-        value = load_from_json(store_name, store_scope, key)
-    elif storage_type == "sql_server":
-        value = load_from_sql_server(store_name, store_scope, key)
-    elif storage_type == "sqlite":
-        value = load_from_sqlite(store_name, store_scope, key)
-    else:
-        return jsonify({'error': 'Invalid storage type'})
-
-    if value is not None:
-        return jsonify({'result': value})
-    else:
-        return jsonify({'error': 'Data not found'})
+        return jsonify({'success': False, 'error': 'Invalid storeScope'})
+    
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
