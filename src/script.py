56ef53c01132f0c0a9d96a9eddaff0869a3c4151
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-24 11:41:56 +0530

    asdasdi hate myself

--- src/script.py
+++ src/script.py
@@ -4,58 +4,70 @@ import sqlite3
 
 app = Quart(__name__)
 
-# Endpoint for loading data
-@app.route('/load', methods=['POST'])
-async def load_data():
-    data = await request.json
-    storeName = data['storeName']
-    storeScope = data['storeScope']
-    key = data['key']
-    
-    # Load data from JSON file or SQLite database based on storeScope
-    if storeScope == 'global':
-        with open('./global_data.json', 'r') as f:
-            global_data = json.load(f)
-        value = global_data.get(key)
-    elif storeScope == 'local':
-        conn = sqlite3.connect('./local_data.db')
-        cursor = conn.cursor()
-        cursor.execute("./SELECT value FROM local_data WHERE storeName=? AND key=?", (storeName, key))
-        result = cursor.fetchone()
-        value = result[0] if result else None
-        conn.close()
-    else:
-        return jsonify({'success': False, 'error': 'Invalid storeScope'})
-    
-    return jsonify({'success': True, 'value': value})
+# Connect to SQLite database
+conn = sqlite3.connect('datastore.db')
+c = conn.cursor()
 
-# Endpoint for saving data
-@app.route('/save', methods=['POST'])
-async def save_data():
-    data = await request.json
-    storeName = data['storeName']
-    storeScope = data['storeScope']
-    key = data['key']
-    value = data['value']
-    
-    # Save data to JSON file or SQLite database based on storeScope
-    if storeScope == 'global':
-        with open('./global_data.json', 'r') as f:
-            global_data = json.load(f)
-        global_data[key] = value
-        with open('./global_data.json', 'w') as f:
-            json.dump(global_data, f)
-    elif storeScope == 'local':
-        conn = sqlite3.connect('./local_data.db')
-        cursor = conn.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS local_data (storeName TEXT, key TEXT, value TEXT)")
-        cursor.execute("INSERT OR REPLACE INTO local_data (storeName, key, value) VALUES (?, ?, ?)", (storeName, key, value))
+# Create a table for data storage
+c.execute('''CREATE TABLE IF NOT EXISTS data_store
+             (key TEXT PRIMARY KEY, value TEXT)''')
+conn.commit()
+
+# Create a JSON file if missing
+try:
+    with open('datastore.json', 'r') as f:
+        pass
+except FileNotFoundError:
+    with open('datastore.json', 'w') as f:
+        json.dump({}, f)
+
+@app.route('/datastore', methods=['POST'])
+async def store_data():
+    try:
+        data = await request.get_json()
+        if 'key' not in data or 'value' not in data:
+            return 'Error: Invalid payload', 400
+
+        key = data['key']
+        value = data['value']
+
+        # Store data in JSON file
+        with open('datastore.json', 'r+') as f:
+            datastore = json.load(f)
+            datastore[key] = value
+            f.seek(0)
+            json.dump(datastore, f, indent=4)
+
+        # Store data in SQLite database
+        c.execute("INSERT OR REPLACE INTO data_store (key, value) VALUES (?, ?)",
+                  (key, json.dumps(value)))
         conn.commit()
-        conn.close()
-    else:
-        return jsonify({'success': False, 'error': 'Invalid storeScope'})
-    
-    return jsonify({'success': True})
+
+        return 'Success', 200
+    except Exception as e:
+        return f'Error: {e}', 500
+
+
+@app.route('/datastore/<key>', methods=['GET'])
+async def get_data(key):
+    try:
+        # Retrieve data from JSON file
+        with open('datastore.json', 'r') as f:
+            datastore = json.load(f)
+            value_json = datastore.get(key)
+
+        # Retrieve data from SQLite database
+        c.execute("SELECT value FROM data_store WHERE key=?", (key,))
+        value_db = c.fetchone()
+
+        if value_json is None and value_db is None:
+            return 'Error: Key not found', 404
+
+        # Return data as JSON
+        return jsonify({'key': key, 'value_json': value_json, 'value_db': json.loads(value_db[0])}), 200
+    except Exception as e:
+        return f'Error: {e}', 500
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8001)
+    app.run(debug=True)
