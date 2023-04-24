199d8cb9125a6f03159afe13c19f874fa7d48c31
LeontKing2 <84083235+LeontKing2@users.noreply.github.com>
2023-04-24 11:38:25 +0530

    I dont know what i am doing anymore

--- thisistheluascript.lua
+++ thisistheluascript.lua
@@ -1,135 +1,111 @@
-local HttpService = game:GetService("HttpService")
+local DataStoreService = {}
 local API = {}
 local MT = {}
+local HttpService = game:GetService("HttpService")
+
+-----------------------------------------------------------------------------------------------------------
+
+local realDataStoreService = game:GetService("DataStoreService")
+local allStores = {}
+
+if (game.Players.LocalPlayer) then
+    warn("Mocked DataStoreService is functioning on the client: The real DataStoreService will not work on the client")
+end
+
 
 -----------------------------------------------------------------------------------------------------------
 -- API:
 
 function API:GetDataStore(name, scope)
-   assert(type(name) == "string", "DataStore name must be a string; got " .. type(name))
-   assert(type(scope) == "string" or scope == nil, "DataStore scope must be a string; got " .. type(scope))
-   scope = (scope or "global")
-   local data = {}
-   local d = {}
-   function d:SetAsync(k, v)
-      assert(v ~= nil, "Value cannot be nil")
-      data[k] = v
-      self:_SendDataToServer(name, scope, k, v)
-   end
-   function d:UpdateAsync(k, func)
-      local v = func(data[k])
-      assert(v ~= nil, "Value cannot be nil")
-      data[k] = v
-      self:_SendDataToServer(name, scope, k, v)
-   end
-   function d:GetAsync(k)
-      if data[k] ~= nil then
-         return data[k]
-      else
-         local response = self:_GetDataFromServer(name, scope, k)
-         if response and response.success then
-            data[k] = response.value
-            return response.value
-         else
-            return nil
-         end
-      end
-   end
-   function d:IncrementAsync(k, delta)
-      if (delta == nil) then delta = 1 end
-      assert(type(delta) == "number", "Can only increment numbers")
-      self:UpdateAsync(k, function(num)
-      if (num == nil) then
-         return num
-      end
-      assert(type(num) == "number", "Can only increment numbers")
-      return (num + delta)
-      end)
-   end
-   return d
+    assert(type(name) == "string", "DataStore name must be a string; got" .. type(name))
+    assert(type(scope) == "string" or scope == nil, "DataStore scope must be a string; got" .. type(scope))
+    scope = (scope or "global")
+    if (allStores[scope] and allStores[scope][name]) then
+        return allStores[scope][name]
+    end
+    local data = {}
+    local d = {}
+    local updateListeners = {}
+    function d:SetAsync(k, v)
+        assert(v ~= nil, "Value cannot be nil")
+        data[k] = v
+        if (updateListeners[k]) then
+            for _,f in pairs(updateListeners[k]) do
+                spawn(function() f(v) end)
+            end
+        end
+        -- Send data to HTTP server
+        local payload = {key = k, value = v}
+        local success, err = pcall(function()
+            HttpService:PostAsync("https://example.com/datastore", HttpService:JSONEncode(payload))
+        end)
+        if not success then
+            error("Failed to send data to HTTP server: " .. err)
+        end
+    end
+    function d:UpdateAsync(k, func)
+        local v = func(data[k])
+        assert(v ~= nil, "Value cannot be nil")
+        data[k] = v
+        if (updateListeners[k]) then
+            for _,f in pairs(updateListeners[k]) do
+                spawn(function() f(v) end)
+            end
+        end
+        -- Send data to HTTP server
+        local payload = {key = k, value = v}
+        local success, err = pcall(function()
+            HttpService:PostAsync("https://example.com/datastore", HttpService:JSONEncode(payload))
+        end)
+        if not success then
+            error("Failed to send data to HTTP server: " .. err)
+        end
+    end
+    function d:GetAsync(k)
+        return data[k]
+    end
+    function d:IncrementAsync(k, delta)
+        if (delta == nil) then delta = 1 end
+        assert(type(delta) == "number", "Can only increment numbers")
+        self:UpdateAsync(k, function(num)
+            if (num == nil) then
+                return num
+            end
+            assert(type(num) == "number", "Can only increment numbers")
+            return (num + delta)
+        end)
+    end
+    function d:OnUpdate(k, onUpdateFunc)
+        assert(type(onUpdateFunc) == "function", "Update function argument must be a function")
+        if (not updateListeners[k]) then
+            updateListeners[k] = {onUpdateFunc}
+        else
+            table.insert(updateListeners[k], onUpdateFunc)
+        end
+    end
+    if (not allStores[scope]) then
+        allStores[scope] = {}
+    end
+    allStores[scope][name] = d
+    return d
 end
 
 function API:GetGlobalDataStore()
-   return self:GetDataStore("global", "global")
+    return self:GetDataStore("GlobalDataStore", "global")
 end
-
-function API:_SendDataToServer(storeName, storeScope, key, value)
-   local payload = {
-      storeName = storeName,
-      storeScope = storeScope,
-      key = key,
-      value = value
-   }
-   local success, response = pcall(function()
-   return HttpService:PostAsync("https://datastore.abhidjt.com/save", HttpService:JSONEncode(payload))
-   end)
-   if success then
-      local decodedResponse = HttpService:JSONDecode(response)
-      if decodedResponse and decodedResponse.success then
-         return true
-      else
-         error("Failed to send data to server: " .. (decodedResponse and decodedResponse.error or "Unknown error"))
-      end
-   else
-      error("Failed to send data to server: " .. response)
+   
+   -- Metatable:
+   
+MT.__index = function(self, key)
+if (API[key]) then
+   return API[key]
+else
+   error("Invalid DataStoreService API: " .. key)
    end
 end
-
-function API:_GetDataFromServer(storeName, storeScope, key)
-   local payload = {
-      storeName = storeName,
-      storeScope = storeScope,
-      key = key
-   }
-   local success, response = pcall(function()
-   return HttpService:PostAsync("https://datastore.abhidjt.com/load", HttpService:JSONEncode(payload))
-   end)
-   if success then
-      local decodedResponse = HttpService:JSONDecode(response)
-      if decodedResponse and decodedResponse.success then
-         return decodedResponse
-      else
-         error("Failed to get data from server: " .. (decodedResponse and decodedResponse.error or "Unknown error"))
-      end
-   else
-      error("Failed to get data from server: " .. response)
-   end
-end
-
--- Metatable:
-
-function MT:__index(k)
-   return self:GetAsync(k)
-end
-
-function MT:__newindex(k, v)
-   self:SetAsync(k, v)
-end
-
-function MT:__call(k, v)
-   if type(k) == "function" then
-      k(self)
-   elseif type(v) == "function" then
-      v(self)
-   end
-end
-
--- DataStore Class:
-
-local DataStore = {}
-DataStore.__index = DataStore
-
-function DataStore.new(name, scope)
-   local self = setmetatable({}, DataStore)
-   self.api = setmetatable({}, MT)
-   self.api._GetDataFromServer = API._GetDataFromServer
-   self.api._SendDataToServer = API._SendDataToServer
-   self.api.GetGlobalDataStore = API.GetGlobalDataStore
-   self.api.GetDataStore = API.GetDataStore
-   self.api[name] = self.api:GetDataStore(name, scope)
-   return self
-end
-
--- Return API:
-
-return DataStore
+   
+setmetatable(DataStoreService, MT)
+   
+   -- Return mocked DataStoreService:
+   
+return DataStoreService
